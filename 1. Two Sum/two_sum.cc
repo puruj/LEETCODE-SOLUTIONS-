5ff85305d18218475684910ad2c16fe0cbd28c49
puruj <puru123456789@hotmail.com>
2023-02-13 13:27:45 -0500

    Update two_sum.cc

--- 1. Two Sum/two_sum.cc
+++ 1. Two Sum/two_sum.cc
@@ -2,6 +2,11 @@
 #include <unordered_map>
 using namespace std;
 
+/*
+ Time complexity: O(n) where n is the size of the input vector 'nums'. This is because in the worst case, we need to iterate through the entire vector once to find the two elements that add up to the target value.
+ Space complexity: O(n) because we use an unordered_map to store the elements in the vector along with their indices. In the worst case, the size of the map will be the same as the size of the vector.
+*/
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
